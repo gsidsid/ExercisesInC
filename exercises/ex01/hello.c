5f83c0ae29b1bb266623bee2d7b64c2e4b703fd4
Siddharth Garimella <gsidsid@gmail.com>
2019-01-27 17:53:54 -0500

    completed optimization writeup

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,7 +1,61 @@
+/*
+ * hello.c
+ *
+ * Optimization in C Sandbox
+ *
+ * Siddharth Garimella
+ */
 #include <stdio.h>
 
+/*
+
+    Two versions of this file were compiled and run, one with
+    the -O2 flag and one without it. Assembly code was produced
+    for each and diffed to analyze how optimization works.
+
+    Roughly speaking, the optimized version of hello.c had fewer
+    assembly instructions than the unoptimized version (wc -l 
+    counted 4 fewer lines in the optimized assembly code). 
+
+    The important differences are as follows:
+
+    Instead of reserving 32 bytes of space on the stack for x and y,
+    the optimized version reserved only 16 bytes, just for y.
+
+    -       subl    $32, %esp
+    ---
+    +       subl    $16, %esp
+
+    In the unoptimized version, 5 was put into %esp, and output to %eax.
+    Then, as per the code, 1 was added to %eax (5), and %eax was our result, 6.
+    Finally, y's value, 6, was stored in %esp.
+
+    The optimized version stored nothing but the result, 6 to y. Who needed x, anyway?
+
+    -       movl    $5, 28(%esp)
+    -       movl    28(%esp), %eax
+    -       addl    $1, %eax
+    -       movl    %eax, 24(%esp)
+    -       movl    24(%esp), %eax
+    -       movl    %eax, 4(%esp)
+    ---
+    +       movl    $6, 4(%esp)
+
+    This is just changing the way %eax is reset to zero. I think the xor instruction
+    is often faster than most other instructions, in this case it is better than mov.
+
+    I wonder why it defaulted to mov. My guess is that it's the "return 0" line.
+
+    -       movl    $0, %eax
+    ---
+    +       xorl    %eax, %eax
+
+
+*/
 int main() {
     int x = 5;
-    printf("Hello, World!\n");
+    int y = x + 1;
+    printf("y: %d",y);
     return 0;
 }
+
--- exercises/ex01/hello_opt.s
+++ exercises/ex01/hello_opt.s
@@ -3,12 +3,13 @@
 	.def	___main;	.scl	2;	.type	32;	.endef
 	.section .rdata,"dr"
 LC0:
-	.ascii "Hello, World!\0"
-	.text
+	.ascii "y: %d\0"
+	.section	.text.startup,"x"
+	.p2align 4,,15
 	.globl	_main
 	.def	_main;	.scl	2;	.type	32;	.endef
 _main:
-LFB11:
+LFB13:
 	.cfi_startproc
 	pushl	%ebp
 	.cfi_def_cfa_offset 8
@@ -16,17 +17,17 @@ LFB11:
 	movl	%esp, %ebp
 	.cfi_def_cfa_register 5
 	andl	$-16, %esp
-	subl	$32, %esp
+	subl	$16, %esp
 	call	___main
-	movl	$5, 28(%esp)
+	movl	$6, 4(%esp)
 	movl	$LC0, (%esp)
-	call	_puts
-	movl	$0, %eax
+	call	_printf
+	xorl	%eax, %eax
 	leave
 	.cfi_restore 5
 	.cfi_def_cfa 4, 4
 	ret
 	.cfi_endproc
-LFE11:
+LFE13:
 	.ident	"GCC: (MinGW.org GCC-8.2.0-3) 8.2.0"
-	.def	_puts;	.scl	2;	.type	32;	.endef
+	.def	_printf;	.scl	2;	.type	32;	.endef
