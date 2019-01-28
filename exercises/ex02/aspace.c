ce2bb636ea73c90acbb86014b623249a18597cdf
Siddharth Garimella <gsidsid@gmail.com>
2019-01-28 17:57:59 -0500

    aspace done'

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -10,17 +10,26 @@ License: GNU GPLv3
 
 int var1;
 
+int printLocalVarAddress() {
+    int local = 2;
+    printf("Address of local is: %p\n", &local);
+}
+
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
+    void *np = malloc(128);
     char *s = "Hello, World";
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
+    printf ("np points to %p\n", np);
     printf ("s points to %p\n", s);
+
+    printLocalVarAddress();
 
     return 0;
 }
